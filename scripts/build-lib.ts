fe3c0883f2dd301622c5fe94dfaa0ca42d4f1946
Michael Naumov <mnaoumov@gmail.com>
2024-08-14 13:18:54 -0600

    Fix cjs imports

--- scripts/build-lib.ts
+++ scripts/build-lib.ts
@@ -40,6 +40,7 @@ const buildOptions: BuildOptions = {
           let contents = await readFile(args.path, "utf8");
           contents = contents.replace(/import\.meta\.url/g, "__filename");
           contents = contents.replace("${NODE_PACKAGE_VERSION}", npmPackage.version);
+          contents = contents.replace(/(from\s+["'].*)\.ts(["'];?)/g, "$1.cjs$2");
 
           return {
             contents,
