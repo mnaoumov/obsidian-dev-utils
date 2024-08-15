a93c107e45faaba0da86d57dba29ae3c1c304328
Michael Naumov <mnaoumov@gmail.com>
2024-08-14 22:10:48 -0600

    Fix static build

--- scripts/build-static.ts
+++ scripts/build-static.ts
@@ -3,11 +3,11 @@ import { join } from "node:path/posix";
 import { readdirPosix } from "../src/Fs.ts";
 import { trimStart } from "../src/String.ts";
 
-for (const dirent of await readdirPosix("./static", { withFileTypes: true, recursive: true })) {
+for (const dirent of await readdirPosix("static", { withFileTypes: true, recursive: true })) {
   if (!dirent.isFile()) {
     continue;
   }
 
-  const path = join(trimStart(dirent.parentPath, "static/", true), dirent.name);
+  const path = trimStart(join(dirent.parentPath, dirent.name), "static/");
   await cp(join("static", path), join("dist", path));
 }
