173998f6dbabbdd8932124009f7e765f77e0d825
Michael Naumov <mnaoumov@gmail.com>
2024-08-16 17:13:27 -0600

    Register asyncErrorEventHandler

---
+++ src/obsidian/Plugin/PluginBase.ts
@@ -0,0 +1,21 @@
+import {
+  Notice,
+  Plugin
+} from "obsidian";
+import { registerAsyncErrorEventHandler } from "../../Error.ts";
+
+export abstract class PluginBase extends Plugin {
+  private notice?: Notice;
+
+  public override async onload(): Promise<void> {
+    this.register(registerAsyncErrorEventHandler(() => { this.showNotice("An unhandled error occurred. Please check the console for more information.") }));
+  }
+
+  protected showNotice(message: string): void {
+    if (this.notice) {
+      this.notice.hide();
+    }
+
+    this.notice = new Notice(`${this.manifest.name}\n${message}`);
+  }
+}
