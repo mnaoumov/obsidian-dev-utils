f662d2bbe1e9e992dde3703155e8f71ded158c8f
Michael Naumov <mnaoumov@gmail.com>
2024-08-16 17:44:28 -0600

    PluginBase

--- src/obsidian/Plugin/PluginBase.ts
+++ src/obsidian/Plugin/PluginBase.ts
@@ -6,11 +6,27 @@ import { registerAsyncErrorEventHandler } from "../../Error.ts";
 
 export abstract class PluginBase extends Plugin {
   private notice?: Notice;
+  private _abortSignal!: AbortSignal;
 
-  public override onload(): void {
+  protected get abortSignal(): AbortSignal {
+    return this._abortSignal;
+  }
+
+  public override async onload(): Promise<void> {
     this.register(registerAsyncErrorEventHandler(() => {
       this.showNotice("An unhandled error occurred. Please check the console for more information.");
     }));
+
+    const abortController = new AbortController();
+    this._abortSignal = abortController.signal;
+    this.register(() => abortController.abort());
+    await this.onloadComplete();
+    this.app.workspace.onLayoutReady(() => this.onLayoutReady());
+  }
+
+  protected abstract onloadComplete(): Promise<void>;
+
+  protected onLayoutReady(): void {
   }
 
   protected showNotice(message: string): void {
