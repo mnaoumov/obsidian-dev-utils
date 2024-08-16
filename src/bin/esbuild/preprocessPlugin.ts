544112cee0507c839418996fe1ff7596e2f373b8
Michael Naumov <mnaoumov@gmail.com>
2024-08-15 23:41:41 -0600

    Fix import.meta.url

--- src/bin/esbuild/preprocessPlugin.ts
+++ src/bin/esbuild/preprocessPlugin.ts
@@ -7,8 +7,8 @@ export function preprocessPlugin(): Plugin {
     setup(build): void {
       build.onLoad({ filter: /\.(js|ts|cjs|mjs|cts|mts)$/ }, async (args) => {
         let contents = await readFile(args.path, "utf8");
-        if (contents.includes("import.meta.url")) {
-          contents = "const import_meta_url = require(\"node:url\").pathToFileURL(__filename);\n" + contents.replaceAll("import.meta.url", "import_meta_url");
+        if (contents.match(/import\.meta\.url/)) {
+          contents = "const import_meta_url = require(\"node:url\").pathToFileURL(__filename);\n" + contents.replaceAll(/import\.meta\.url/g, "import_meta_url");
         }
         // HACK: The ${""} part is used to ensure Obsidian loads the plugin properly otherwise it stops loading it after the first line of the sourceMappingURL comment.
         contents = contents.replace(/\`\r?\n\/\/# sourceMappingURL/g, "`\n//#${\"\"} sourceMappingURL");
