0670ce7e731e097af1352e580e6d1a353b29e164
Michael Naumov <mnaoumov@gmail.com>
2024-08-22 10:17:39 -0600

    Add getApp

---
+++ src/obsidian/App.ts
@@ -0,0 +1,43 @@
+/**
+ * @file Provides a utility to retrieve the Obsidian `App` instance.
+ */
+
+import type { App } from "obsidian";
+
+/**
+ * Wrapper type for accessing the `App` instance globally.
+ */
+type AppWrapper = {
+  /**
+   * An optional reference to the Obsidian `App` instance.
+   */
+  app?: App;
+};
+
+/**
+ * Retrieves the Obsidian `App` instance.
+ *
+ * @returns The `App` instance.
+ * @throws Will throw an error if the `App` instance cannot be found.
+ *
+ * @see {@link https://github.com/mnaoumov/obsidian-fix-require-modules/?tab=readme-ov-file#obsidianapp-module}
+ */
+export function getApp(): App {
+  let canRequire = false;
+  try {
+    globalThis.require.resolve("obsidian/app");
+    canRequire = true;
+  } catch {
+  }
+
+  if (canRequire) {
+    return globalThis.require("obsidian/app") as App;
+  }
+
+  const app = (globalThis as AppWrapper).app;
+  if (app) {
+    return app;
+  }
+
+  throw new Error("Obsidian app not found");
+}
