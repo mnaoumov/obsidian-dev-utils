dba8fc8ec4f3722183348c7c6d02a15ba737c6de
Michael Naumov <mnaoumov@gmail.com>
2024-08-24 16:53:23 -0600

    Check for required tsdoc

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -3,18 +3,20 @@ import { wrapCliTask } from "../src/cli.ts";
 import process from "node:process";
 import eslintPluginTsdoc from "eslint-plugin-tsdoc";
 import eslintPluginVerifyTsdoc from "eslint-plugin-verify-tsdoc";
+import eslintPluginTsdocRequired from "@guardian/eslint-plugin-tsdoc-required";
 
 await wrapCliTask(async () => {
   const fix = process.argv[2] === "fix";
   return await lint(fix, [{
     plugins: {
       "tsdoc": eslintPluginTsdoc,
-      "verify-tsdoc": eslintPluginVerifyTsdoc
+      "verify-tsdoc": eslintPluginVerifyTsdoc,
+      "eslint-plugin-tsdoc-required": eslintPluginTsdocRequired
     },
     rules: {
       "tsdoc/syntax": "error",
-      //"verify-tsdoc/tsdoc-comments-required": "error",
-      "verify-tsdoc/verify-tsdoc-params": "error"
+      "verify-tsdoc/verify-tsdoc-params": "error",
+      "eslint-plugin-tsdoc-required/tsdoc-required": "error",
     }
   }]);
 });
---
+++ src/bin/ESLint/@types/@guardian__eslint-plugin-tsdoc-required.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @packageDocumentation \@guardian/eslint-plugin-tsdoc-required
+ * @see {@link https://www.npmjs.com/package/@guardian/eslint-plugin-tsdoc-required} for more information.
+ */
+
+declare module "@guardian/eslint-plugin-tsdoc-required" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
