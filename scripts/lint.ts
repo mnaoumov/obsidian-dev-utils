d85f757ed5f1a2adf49aff670ae71c4996e059d7
Michael Naumov <mnaoumov@gmail.com>
2024-08-24 16:10:06 -0600

    Add tsdoc validator

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -2,15 +2,19 @@ import { lint } from "../src/bin/ESLint/ESLint.ts";
 import { wrapCliTask } from "../src/cli.ts";
 import process from "node:process";
 import eslintPluginTsdoc from "eslint-plugin-tsdoc";
+import eslintPluginVerifyTsdoc from "eslint-plugin-verify-tsdoc";
 
 await wrapCliTask(async () => {
   const fix = process.argv[2] === "fix";
   return await lint(fix, [{
     plugins: {
-      "tsdoc": eslintPluginTsdoc
+      "tsdoc": eslintPluginTsdoc,
+      "verify-tsdoc": eslintPluginVerifyTsdoc
     },
     rules: {
-      "tsdoc/syntax": "error"
+      "tsdoc/syntax": "error",
+      //"verify-tsdoc/tsdoc-comments-required": "error",
+      "verify-tsdoc/verify-tsdoc-params": "error"
     }
   }]);
 });
---
+++ src/bin/ESLint/@types/eslint-plugin-verify-tsdoc.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @packageDocumentation eslint-plugin-verify-tsdoc
+ * @see {@link https://www.npmjs.com/package/eslint-plugin-verify-tsdoc} for more information.
+ */
+
+declare module "eslint-plugin-verify-tsdoc" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
