f38d3a042014473be0028eef6f1d8f454719218a
Michael Naumov <mnaoumov@gmail.com>
2024-08-25 10:34:40 -0600

    Refactor UIComponent

--- src/obsidian/Plugin/PluginSettingsTabBase.ts
+++ src/obsidian/Plugin/PluginSettingsTabBase.ts
@@ -6,39 +6,6 @@
 
 import { PluginSettingTab } from "obsidian";
 import { PluginBase } from "./PluginBase.ts";
-import type { KeysMatching } from "../../@types.ts";
-
-interface ValueComponent<T> {
-  setValue(value: T): this;
-  onChange(callback: (newValue: T) => Promise<void>): this;
-}
-
-/**
- * Options for binding a value component to a plugin setting.
- */
-type BindValueComponentOptions<PluginSettings, Property extends keyof PluginSettings, UIValueType> = {
-  // If true, saves the plugin settings automatically after the component value changes. Default is `true`.
-  autoSave?: boolean;
-
-  /**
-   * The plugin settings object to bind the component to. Default is the plugin's current settings.
-   */
-  pluginSettings?: PluginSettings;
-
-  /**
-   * Converts the setting value to the value used by the UI component.
-   * @param propertyValue - The value of the property in the plugin settings.
-   * @returns The value to set on the UI component.
-   */
-  settingToUIValueConverter: (propertyValue: PluginSettings[Property]) => UIValueType;
-
-  /**
-   * Converts the UI component's value back to the setting value.
-   * @param uiValue - The value of the UI component.
-   * @returns The value to set on the plugin settings.
-   */
-  uiToSettingValueConverter: (uiValue: UIValueType) => PluginSettings[Property];
-};
 
 /**
  * Base class for creating plugin settings tabs in Obsidian.
@@ -49,95 +16,10 @@ type BindValueComponentOptions<PluginSettings, Property extends keyof PluginSett
  */
 export abstract class PluginSettingsTabBase<
   TPlugin extends PluginBase<PluginSettings>,
-  PluginSettings extends object
+  PluginSettings extends object = TPlugin extends PluginBase<infer P> ? P : never,
 > extends PluginSettingTab {
   public constructor(public override plugin: TPlugin) {
     super(plugin.app, plugin);
   }
-
-  /**
-   * Binds a value component to a property in the plugin settings with optional automatic saving and value conversion.
-   *
-   * @typeParam TValueComponent - The type of the value component extending `ValueComponent`.
-   * @typeParam Property - The key of the plugin setting that the component is bound to.
-   * @typeParam PropertyType - The inferred type of the property based on the value component's type.
-   *
-   * @param valueComponent - The component that will display and interact with the setting value.
-   * @param property - The property key in `PluginSettings` to bind to the component.
-   * @param options - Configuration options.
-   *
-   * @returns The `TValueComponent` instance that was bound to the property.
-   */
-  protected bindValueComponent<
-    TValueComponent extends ValueComponent<unknown>,
-    Property extends KeysMatching<PluginSettings, UIValueType>,
-    UIValueType = TValueComponent extends ValueComponent<infer P> ? P : never,
-  >(
-    valueComponent: TValueComponent,
-    property: Property,
-    options?: BindValueComponentOptions<PluginSettings, Property, UIValueType>
-  ): TValueComponent;
-
-  /**
-   * Binds a value component to a property in the plugin settings with automatic saving and custom value conversion.
-   *
-   * @typeParam TValueComponent - The type of the value component extending `ValueComponent`.
-   * @typeParam Property - The key of the plugin setting that the component is bound to.
-   * @typeParam UIValueType - The inferred type of the UI value based on the value component's type.
-   *
-   * @param valueComponent - The component that will display and interact with the setting value.
-   * @param property - The property key in `PluginSettings` to bind to the component.
-   * @param options - Configuration options.
-   *
-   * @returns The `TValueComponent` instance that was bound to the property.
-   */
-  protected bindValueComponent<
-    TValueComponent extends ValueComponent<unknown>,
-    Property extends keyof PluginSettings,
-    UIValueType = TValueComponent extends ValueComponent<infer P> ? P : never,
-  >(
-    valueComponent: TValueComponent,
-    property: Property,
-    options: BindValueComponentOptions<PluginSettings, Property, UIValueType>
-  ): TValueComponent;
-
-  /**
-   * Internal implementation of `bindValueComponent` that handles both overloads.
-   *
-   * @typeParam TValueComponent - The type of the value component extending `ValueComponent`.
-   * @typeParam Property - The key of the plugin setting that the component is bound to.
-   * @typeParam UIValueType - The inferred type of the UI value based on the value component's type.
-   *
-   * @param valueComponent - The component that will display and interact with the setting value.
-   * @param property - The property key in `PluginSettings` to bind to the component.
-   * @param options - Configuration options.
-   *
-   * @returns The `TValueComponent` instance that was bound to the property.
-   */
-  protected bindValueComponent<
-    TValueComponent extends ValueComponent<unknown>,
-    Property extends keyof PluginSettings,
-    UIValueType = TValueComponent extends ValueComponent<infer P> ? P : never,
-  >(
-    valueComponent: TValueComponent,
-    property: Property,
-    options: BindValueComponentOptions<PluginSettings, Property, UIValueType>
-  ): TValueComponent {
-    const {
-      autoSave,
-      pluginSettings,
-      settingToUIValueConverter,
-      uiToSettingValueConverter
-    } = options;
-    const pluginSettings2 = pluginSettings ?? this.plugin.settingsCopy;
-    (valueComponent as ValueComponent<UIValueType>)
-      .setValue(settingToUIValueConverter(pluginSettings2[property]))
-      .onChange(async (newValue) => {
-        pluginSettings2[property] = uiToSettingValueConverter(newValue);
-        if (autoSave) {
-          await this.plugin.saveSettings(pluginSettings2);
-        }
-      });
-    return valueComponent;
-  }
 }
+
---
+++ src/obsidian/Plugin/UIComponent.ts
@@ -0,0 +1,220 @@
+import type { KeysMatching } from "../../@types.ts";
+import type { PluginSettingsTabBase } from "./PluginSettingsTabBase.ts";
+import type { PluginBase } from "./PluginBase.ts";
+import {
+  DropdownComponent,
+  SliderComponent,
+  TextAreaComponent,
+  TextComponent
+} from "obsidian";
+
+/**
+ * A UI component that can be bound to a plugin setting.
+ */
+export interface UIComponent<UIValue> {
+  /**
+   * Sets the value of the component.
+   * @param value - The value to set on the component.
+   */
+  setValue(value: UIValue): this;
+
+  /**
+   * Sets a callback function to be called when the value of the component changes.
+   * @param callback - A callback function that is called when the value of the component changes.
+   */
+  onChange(callback: (newValue: UIValue) => Promise<void>): this;
+}
+
+/**
+ * A UI component that can be validated.
+ */
+interface ValidatorElement {
+  /**
+   * Sets a custom error message on the element.
+   * @param error - The error message to set on the element.
+   */
+  setCustomValidity(error: string): void;
+
+  /**
+   * Reports the validity of the element.
+   */
+  reportValidity(): boolean;
+}
+
+/**
+ * Options for binding a value component to a plugin setting.
+ */
+type BindUIComponentOptions<PluginSettings, Property extends keyof PluginSettings, UIValueType> = {
+  // If true, saves the plugin settings automatically after the component value changes. Default is `true`.
+  autoSave?: boolean;
+
+  /**
+   * The plugin settings object to bind the component to. Default is the plugin's current settings.
+   */
+  pluginSettings?: PluginSettings;
+
+  /**
+   * Converts the setting value to the value used by the UI component.
+   * @param propertyValue - The value of the property in the plugin settings.
+   * @returns The value to set on the UI component.
+   */
+  settingToUIValueConverter: (propertyValue: PluginSettings[Property]) => UIValueType;
+
+  /**
+   * Converts the UI component's value back to the setting value.
+   * @param uiValue - The value of the UI component.
+   * @returns The value to set on the plugin settings.
+   */
+  uiToSettingValueConverter: (uiValue: UIValueType) => PluginSettings[Property];
+
+  /**
+   * Validates the UI value before setting it on the plugin settings.
+   * @param uiValue - The value of the UI component.
+   * @returns An error message if the value is invalid, or `null` if it is valid.
+   */
+  uiValueValidator?: (uiValue: UIValueType) => string | null;
+};
+
+/**
+ * Binds a value component to a property in the plugin settings with optional automatic saving and value conversion.
+ *
+ * @typeParam PluginSettingsTab - The type of the plugin settings tab that extends `PluginSettingsTabBase`.
+ * @typeParam TUIComponent - The type of the value component extending `UIComponent`.
+ * @typeParam Property - The key of the plugin setting that the component is bound to.
+ * @typeParam UIValueType - The inferred type based on the UI component's type.
+ * @typeParam TPlugin - The inferred type of the plugin that extends `PluginBase`.
+ * @typeParam PluginSettings - The inferred type of the plugin settings object.
+ *
+ * @param pluginSettingsTab - The plugin settings tab that contains the UI component.
+ * @param uiComponent - The component that will display and interact with the setting value.
+ * @param property - The property key in `PluginSettings` to bind to the UI component.
+ * @param options - Configuration options.
+ *
+ * @returns The `UIComponent` instance that was bound to the property.
+ */
+export function bindUiComponent<
+  PluginSettingsTab extends PluginSettingsTabBase<TPlugin, PluginSettings>,
+  TUIComponent extends UIComponent<unknown>,
+  Property extends KeysMatching<PluginSettings, UIValueType>,
+  UIValueType = TUIComponent extends UIComponent<infer P> ? P : never,
+  TPlugin extends PluginBase<PluginSettings> = PluginSettingsTab extends PluginSettingsTabBase<infer P> ? P : never,
+  PluginSettings extends object = TPlugin extends PluginBase<infer P> ? P : never,
+>(
+  pluginSettingsTab: PluginSettingsTab,
+  uiComponent: TUIComponent,
+  property: Property,
+  options?: BindUIComponentOptions<PluginSettings, Property, UIValueType>
+): TUIComponent;
+
+/**
+ * Binds a value component to a property in the plugin settings with optional automatic saving and value conversion.
+ *
+ * @typeParam PluginSettingsTab - The type of the plugin settings tab that extends `PluginSettingsTabBase`.
+ * @typeParam TUIComponent - The type of the value component extending `UIComponent`.
+ * @typeParam Property - The key of the plugin setting that the component is bound to.
+ * @typeParam UIValueType - The inferred type based on the UI component's type.
+ * @typeParam TPlugin - The inferred type of the plugin that extends `PluginBase`.
+ * @typeParam PluginSettings - The inferred type of the plugin settings object.
+ *
+ * @param pluginSettingsTab - The plugin settings tab that contains the UI component.
+ * @param uiComponent - The component that will display and interact with the setting value.
+ * @param property - The property key in `PluginSettings` to bind to the UI component.
+ * @param options - Configuration options.
+ *
+ * @returns The `UIComponent` instance that was bound to the property.
+ */
+export function bindUiComponent<
+  PluginSettingsTab extends PluginSettingsTabBase<TPlugin, PluginSettings>,
+  TUIComponent extends UIComponent<unknown>,
+  Property extends keyof PluginSettings,
+  UIValueType = TUIComponent extends UIComponent<infer P> ? P : never,
+  TPlugin extends PluginBase<PluginSettings> = PluginSettingsTab extends PluginSettingsTabBase<infer P> ? P : never,
+  PluginSettings extends object = TPlugin extends PluginBase<infer P> ? P : never,
+>(
+  pluginSettingsTab: PluginSettingsTab,
+  uiComponent: TUIComponent,
+  property: Property,
+  options: BindUIComponentOptions<PluginSettings, Property, UIValueType>
+): TUIComponent;
+
+/**
+ * Binds a value component to a property in the plugin settings with optional automatic saving and value conversion.
+ *
+ * @typeParam PluginSettingsTab - The type of the plugin settings tab that extends `PluginSettingsTabBase`.
+ * @typeParam TUIComponent - The type of the value component extending `UIComponent`.
+ * @typeParam Property - The key of the plugin setting that the component is bound to.
+ * @typeParam UIValueType - The inferred type based on the UI component's type.
+ * @typeParam TPlugin - The inferred type of the plugin that extends `PluginBase`.
+ * @typeParam PluginSettings - The inferred type of the plugin settings object.
+ *
+ * @param pluginSettingsTab - The plugin settings tab that contains the UI component.
+ * @param uiComponent - The component that will display and interact with the setting value.
+ * @param property - The property key in `PluginSettings` to bind to the UI component.
+ * @param options - Configuration options.
+ *
+ * @returns The `UIComponent` instance that was bound to the property.
+ */
+export function bindUiComponent<
+  PluginSettingsTab extends PluginSettingsTabBase<TPlugin, PluginSettings>,
+  TUIComponent extends UIComponent<unknown>,
+  Property extends keyof PluginSettings,
+  UIValueType = TUIComponent extends UIComponent<infer P> ? P : never,
+  TPlugin extends PluginBase<PluginSettings> = PluginSettingsTab extends PluginSettingsTabBase<infer P> ? P : never,
+  PluginSettings extends object = TPlugin extends PluginBase<infer P> ? P : never,
+>(
+  pluginSettingsTab: PluginSettingsTab,
+  uiComponent: TUIComponent,
+  property: Property,
+  options?: BindUIComponentOptions<PluginSettings, Property, UIValueType>
+): TUIComponent {
+  options ??= {
+    settingToUIValueConverter: (value): UIValueType => value as UIValueType,
+    uiToSettingValueConverter: (value): PluginSettings[Property] => value as PluginSettings[Property],
+  };
+  const pluginSettings = options.pluginSettings ?? pluginSettingsTab.plugin.settingsCopy;
+  (uiComponent as UIComponent<UIValueType>)
+    .setValue(options.settingToUIValueConverter(pluginSettings[property]))
+    .onChange(async (uiValue) => {
+      if (options.uiValueValidator) {
+        const errorMessage = options.uiValueValidator(uiValue);
+        const validatorElement = getValidatorElement(uiComponent);
+        if (validatorElement) {
+          validatorElement.setCustomValidity(errorMessage ?? "");
+          validatorElement.reportValidity();
+        }
+        if (errorMessage) {
+          return;
+        }
+      }
+      pluginSettings[property] = options.uiToSettingValueConverter(uiValue);
+      if (options.autoSave ?? true) {
+        await pluginSettingsTab.plugin.saveSettings(pluginSettings);
+      }
+    });
+  return uiComponent;
+}
+
+/**
+ * Gets the validator element from a UI component if it exists.
+ * @param uiComponent - The UI component to get the validator element from.
+ * @returns The validator element if it exists, or `null` if it does not.
+ */
+function getValidatorElement(uiComponent: UIComponent<unknown>): ValidatorElement | null {
+  if (uiComponent instanceof DropdownComponent) {
+    return uiComponent.selectEl;
+  }
+
+  if (uiComponent instanceof SliderComponent) {
+    return uiComponent.sliderEl;
+  }
+
+  if (uiComponent instanceof TextAreaComponent) {
+    return uiComponent.inputEl;
+  }
+
+  if (uiComponent instanceof TextComponent) {
+    return uiComponent.inputEl;
+  }
+
+  return null;
+}
