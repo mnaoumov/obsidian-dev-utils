2f72ff02e40814424461c52bf403f7aa4d4697f8
Michael Naumov <mnaoumov@gmail.com>
2024-11-07 18:10:53 -0600

    Add getFullContentHtml

---
+++ src/obsidian/MarkdownView.ts
@@ -0,0 +1,22 @@
+/**
+ * @packageDocumentation MarkdownView
+ * Utilities for working with MarkdownView
+ */
+
+import { MarkdownView } from 'obsidian';
+
+/**
+ * Get the full HTML content of the current MarkdownView
+ *
+ * @param view - The MarkdownView to get the HTML from
+ * @returns The full HTML of the MarkdownView
+ */
+export function getFullContentHtml(view: MarkdownView): string {
+  const codeMirror = view.editor.cm;
+  codeMirror.viewState.printing = true;
+  codeMirror.measure();
+  const html = view.contentEl.innerHTML;
+  codeMirror.viewState.printing = false;
+  codeMirror.measure();
+  return html;
+}
