afff52eca9191c1c707fc556eb1b82f43e985864
Michael Naumov <mnaoumov@gmail.com>
2024-11-13 12:45:17 -0600

    Refactor getCodeBlockArguments

--- src/obsidian/MarkdownCodeBlockProcessor.ts
+++ src/obsidian/MarkdownCodeBlockProcessor.ts
@@ -5,6 +5,8 @@
 
 import type { MarkdownPostProcessorContext } from 'obsidian';
 
+import { parse } from 'shell-quote';
+
 import { throwExpression } from '../Error.ts';
 
 /**
@@ -14,16 +16,16 @@ import { throwExpression } from '../Error.ts';
  * @param el - The HTMLElement representing the code block.
  * @returns The argument of the code block as a string, or null if no argument is found.
  */
-export function getCodeBlockArgument(ctx: MarkdownPostProcessorContext, el: HTMLElement): null | string {
+export function getCodeBlockArguments(ctx: MarkdownPostProcessorContext, el: HTMLElement): string[] {
   const sectionInfo = ctx.getSectionInfo(el);
   if (!sectionInfo) {
-    return null;
+    return [];
   }
   const lines = sectionInfo.text.split('\n');
   const codeBlockHeader = lines[sectionInfo.lineStart] ?? throwExpression(new Error('Code block header not found'));
   const match = /^`{3,}\S+\s+(.*)$/.exec(codeBlockHeader);
   if (!match) {
-    return null;
+    return [];
   }
-  return match[1]?.trim() ?? null;
+  return parse(match[1] ?? '').map(String);
 }
