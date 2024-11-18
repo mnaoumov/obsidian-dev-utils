2de29cfb084ec443ac50728ac7eaff4459f349f6
Michael Naumov <mnaoumov@gmail.com>
2024-11-17 22:52:07 -0600

    ChainedPromise

--- src/obsidian/ChainedPromise.ts
+++ src/obsidian/ChainedPromise.ts
@@ -10,6 +10,7 @@ import type { ValueWrapper } from './App.ts';
 
 import { addErrorHandler } from '../Async.ts';
 import { getStackTrace } from '../Error.ts';
+import { noop } from '../Function.ts';
 import { getObsidianDevUtilsState } from './App.ts';
 import { invokeAsyncAndLog } from './Logger.ts';
 
@@ -24,7 +25,27 @@ function getChainedPromiseWrapper(app: App): ValueWrapper<Promise<void>> {
  * @param fn - The function to chain.
  */
 export function chain(app: App, fn: () => MaybePromise<void>): void {
+  void chainAndWait(app, fn);
+}
+
+/**
+ * Chains an asynchronous function to be executed after the previous function completes and returns a promise that resolves when the function completes.
+ *
+ * @param app - The Obsidian application instance.
+ * @param fn - The function to chain.
+ */
+export async function chainAndWait(app: App, fn: () => MaybePromise<void>): Promise<void> {
   const stackTrace = getStackTrace();
   const chainedPromiseWrapper = getChainedPromiseWrapper(app);
-  chainedPromiseWrapper.value = chainedPromiseWrapper.value.then(() => addErrorHandler(() => invokeAsyncAndLog('chain', fn, stackTrace)));
+  chainedPromiseWrapper.value = chainedPromiseWrapper.value.then(() => addErrorHandler(() => invokeAsyncAndLog('chainAndWait', fn, stackTrace)));
+  await chainedPromiseWrapper.value;
+}
+
+/**
+ * Waits for all chained functions to complete.
+ *
+ * @param app - The Obsidian application instance.
+ */
+export async function waitAllChainedFunctions(app: App): Promise<void> {
+  await chainAndWait(app, noop);
 }
