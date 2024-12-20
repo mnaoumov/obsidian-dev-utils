b5a78cf872b3bbf8ea59fe7f4a17ae5033495de3
Michael Naumov <mnaoumov@gmail.com>
2024-12-20 15:35:24 -0600

    Pass NPM_TOKEN

--- scripts/version.ts
+++ scripts/version.ts
@@ -2,10 +2,20 @@ import { wrapCliTask } from '../src/scripts/CliUtils.ts';
 import { updateVersion } from '../src/scripts/version.ts';
 import { process } from '../src/scripts/NodeModules.ts';
 import { execFromRoot } from '../src/scripts/Root.ts';
+import { config } from 'dotenv';
+
+interface NpmConfig {
+  NPM_TOKEN: string;
+}
 
 await wrapCliTask(async () => {
   const versionUpdateTypeStr = process.argv[2];
   await updateVersion(versionUpdateTypeStr);
+
+  const dotenvConfigOutput = config();
+  const npmConfig = (dotenvConfigOutput.parsed ?? {}) as Partial<NpmConfig>
+  await execFromRoot(['npm', 'config', 'set', '//registry.npmjs.org/:_authToken', npmConfig.NPM_TOKEN ?? '']);
+
   const tag = versionUpdateTypeStr === 'beta' ? 'beta' : 'latest';
   await execFromRoot(['npm', 'publish', '--tag', tag]);
 });
