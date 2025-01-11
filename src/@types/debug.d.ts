cae971a55d8168b794a69d4b86731aecffd96085
Michael Naumov <mnaoumov@gmail.com>
2025-01-11 11:16:40 -0600

    Refactor DEBUG

---
+++ src/@types/debug.d.ts
@@ -0,0 +1,16 @@
+/**
+ * @packageDocumentation debug
+ * Fixed typings for the `debug` package.
+ */
+
+export { };
+
+declare module 'debug' {
+  interface Debug {
+    /**
+     * Loads the debug configuration from the environment.
+     * @returns The loaded debug configuration.
+     */
+    load(): string;
+  }
+}
