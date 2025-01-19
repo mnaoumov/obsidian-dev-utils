7ea5c4786287ade5effa05b759a39529ea1e4ede
Michael Naumov <mnaoumov@gmail.com>
2025-01-18 19:08:34 -0600

    Embed version

---
+++ src/Library.ts
@@ -0,0 +1,9 @@
+/**
+ * The version of the `obsidian-dev-utils` library.
+ */
+export const LIBRARY_VERSION = '$(LIBRARY_VERSION)';
+
+/**
+ * The name of the `obsidian-dev-utils` library.
+ */
+export const LIBRARY_NAME = 'obsidian-dev-utils';
