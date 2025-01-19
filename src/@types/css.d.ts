8927c3841f0411cdc87c9eb2b19c9803736ba18d
Michael Naumov <mnaoumov@gmail.com>
2025-01-18 19:28:36 -0600

    Register styles

---
+++ src/@types/css.d.ts
@@ -0,0 +1,5 @@
+declare module '*.css' {
+  const content: string;
+  // eslint-disable-next-line import-x/no-default-export
+  export default content;
+}
--- src/obsidian/Plugin/PluginContext.ts
+++ src/obsidian/Plugin/PluginContext.ts
@@ -4,17 +4,23 @@ import { compareVersions } from 'compare-versions';
 
 import type { DebugController } from '../../DebugController.ts';
 
+import stylesCss from '../../../static/styles.css';
 import {
   getDebugController,
   showInitialDebugMessage
 } from '../../Debug.ts';
-import { LIBRARY_VERSION } from '../../Library.ts';
+import {
+  LIBRARY_NAME,
+  LIBRARY_VERSION
+} from '../../Library.ts';
 import { getObsidianDevUtilsState } from '../App.ts';
 import { setPluginId } from './PluginId.ts';
 
 interface PluginContextWindow {
   DEBUG: DebugController;
 }
+
+const STYLES_ID = `${LIBRARY_NAME}-styles`;
 
 /**
  * Initializes the plugin context.
@@ -35,6 +41,14 @@ export function initPluginContext(app: App, pluginId: string): void {
 
   const pluginContextWindow = getPluginContextWindow();
   pluginContextWindow.DEBUG = getDebugController();
+
+  document.head.querySelector(`#${STYLES_ID}`)?.remove();
+  document.head.createEl('style', {
+    attr: {
+      id: STYLES_ID
+    },
+    text: stylesCss
+  });
 }
 
 function getPluginContextWindow(): Partial<PluginContextWindow> {
