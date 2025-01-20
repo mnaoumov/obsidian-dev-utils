ac68c2a1931e474d8eddd62447bbed8ca129b0d7
Michael Naumov <mnaoumov@gmail.com>
2025-01-20 17:51:00 -0600

    Cache results

--- src/scripts/esbuild/watchCssChangesPlugin.ts
+++ src/scripts/esbuild/watchCssChangesPlugin.ts
@@ -11,25 +11,18 @@ import { resolvePathFromRootSafe } from '../Root.ts';
  * @returns A plugin that watches for changes to CSS files and rebuilds the plugin when they change.
  */
 export function watchCssChangesPlugin(): Plugin {
+  const stylesCssPath = resolvePathFromRootSafe(ObsidianPluginRepoPaths.StylesCss);
+  const watchFiles: string[] = [];
+  if (existsSync(stylesCssPath)) {
+    watchFiles.push(stylesCssPath);
+  }
+
   return {
     name: 'watch-css-changes',
     setup(build): void {
-      let isInitialized = false;
-
-      build.onLoad({ filter: /\.*/ }, (): null | OnLoadResult => {
-        if (isInitialized) {
-          return null;
-        }
-
-        isInitialized = true;
-
-        const stylesCssPath = resolvePathFromRootSafe(ObsidianPluginRepoPaths.StylesCss);
-        if (!existsSync(stylesCssPath)) {
-          return null;
-        }
-
+      build.onLoad({ filter: /\.*/ }, (): OnLoadResult => {
         return {
-          watchFiles: [stylesCssPath]
+          watchFiles
         };
       });
     }
