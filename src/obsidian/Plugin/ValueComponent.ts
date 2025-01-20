5abaa7dbfca4c4efaeb3438d8b1e55e93ba879a5
Michael Naumov <mnaoumov@gmail.com>
2025-01-20 14:37:03 -0600

    Refactor bind

--- src/obsidian/Plugin/ValueComponent.ts
+++ src/obsidian/Plugin/ValueComponent.ts
@@ -11,80 +11,12 @@ import {
   ValueComponent
 } from 'obsidian';
 
-import type { KeysMatching } from '../../@types.ts';
-import type { MaybePromise } from '../../Async.ts';
 import type { ValidatorElement } from '../../HTMLElement.ts';
-import type { PluginBase } from './PluginBase.ts';
-import type { PluginSettingsBase } from './PluginSettingsBase.ts';
-
-import {
-  convertAsyncToSync,
-  invokeAsyncSafely
-} from '../../Async.ts';
-import { CssClass } from '../../CssClass.ts';
-import { assignWithNonEnumerableProperties } from '../../Object.ts';
-
-/**
- * Options for binding a value component to a plugin setting.
- */
-interface BindValueComponentOptions<PluginSettings, UIValue> {
-  /**
-   * A callback function that is called when the value of the component changes.
-   */
-  onChanged?: () => MaybePromise<void>;
-
-  /**
-   * The plugin settings object to bind the component to. Default is the plugin's current settings.
-   */
-  pluginSettings?: PluginSettings;
-
-  /**
-   * If true, saves the plugin settings automatically after the component value changes. Default is `true`.
-   */
-  shouldAutoSave?: boolean;
-
-  /**
-   * If true, shows the validation message when the component value is invalid. Default is `true`.
-   */
-  shouldShowValidationMessage?: boolean;
-
-  /**
-   * Validates the UI value before setting it on the plugin settings.
-   * @param uiValue - The value of the UI component.
-   * @returns An error message if the value is invalid, or `(empty string)` or `void` if it is valid.
-   */
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  valueValidator?: (uiValue: UIValue) => MaybePromise<string | void>;
-}
-
-/**
- * Extended options for binding a value component to a plugin setting.
- */
-interface BindValueComponentOptionsExtended<PluginSettings, UIValue, Property extends keyof PluginSettings> extends BindValueComponentOptions<PluginSettings, UIValue> {
-  /**
-   * Converts the UI component's value back to the plugin settings value.
-   * @param uiValue - The value of the UI component.
-   * @returns The value to set on the plugin settings.
-   */
-  componentToPluginSettingsValueConverter: (uiValue: UIValue) => PluginSettings[Property];
-
-  /**
-   * Converts the plugin settings value to the value used by the UI component.
-   * @param pluginSettingsValue - The value of the property in the plugin settings.
-   * @returns The value to set on the UI component.
-   */
-  pluginSettingsToComponentValueConverter: (pluginSettingsValue: PluginSettings[Property]) => UIValue;
-}
-
-/**
- * ValueComponent that can be used as an original ValueComponent with extended functionality.
- */
-type ValueComponentExType<UIValue, TValueComponent extends ValueComponentWithChangeTracking<UIValue>> = TValueComponent & ValueComponentEx<UIValue, TValueComponent>;
 
 /**
  * A ValueComponent that can track changes.
  */
-interface ValueComponentWithChangeTracking<T> extends ValueComponent<T> {
+export interface ValueComponentWithChangeTracking<T> extends ValueComponent<T> {
   /**
    * Sets a callback function to be called when the value of the component changes.
    *
@@ -93,173 +25,13 @@ interface ValueComponentWithChangeTracking<T> extends ValueComponent<T> {
   onChange(callback: (newValue: T) => Promise<void>): this;
 }
 
-const validatorsMap = new WeakMap<ValueComponent<unknown>, () => Promise<boolean>>();
-
-/**
- * ValueComponent with extended functionality.
- */
-class ValueComponentEx<UIValue, TValueComponent extends ValueComponentWithChangeTracking<UIValue>> {
-  public constructor(private valueComponent: TValueComponent) {
-  }
-
-  /**
-   * Returns the ValueComponent with extended functionality.
-   */
-  public asExtended(): ValueComponentExType<UIValue, TValueComponent> {
-    return assignWithNonEnumerableProperties({}, this.valueComponent, this);
-  }
-
-  /**
-   * Binds the ValueComponent to a property in the plugin settings.
-   *
-   * @typeParam PluginSettings - The type of the plugin settings object.
-   * @typeParam Property - The key of the plugin setting that the component is bound to.
-   * @param plugin - The plugin.
-   * @param property - The property key in `PluginSettings` to bind to the UI component.
-   * @param options - Configuration options.
-   * @returns The `ValueComponent` instance that was bound to the property.
-   */
-  public bind<
-    PluginSettings extends PluginSettingsBase,
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-    Property extends KeysMatching<PluginSettings, UIValue>
-  >(
-    plugin: PluginBase<PluginSettings>,
-    property: Property,
-    options?: BindValueComponentOptions<PluginSettings, UIValue>
-  ): ValueComponentExType<UIValue, TValueComponent>;
-
-  /**
-   * Binds the ValueComponent to a property in the plugin settings.
-   *
-   * @typeParam Plugin - The type of the plugin that extends `PluginBase`.
-   * @typeParam Property - The key of the plugin setting that the component is bound to.
-   * @typeParam PluginSettings - The type of the plugin settings object.
-   * @param plugin - The plugin.
-   * @param property - The property key in `PluginSettings` to bind to the UI component.
-   * @param options - Configuration options.
-   * @returns The `ValueComponent` instance that was bound to the property.
-   */
-  public bind<
-    PluginSettings extends PluginSettingsBase,
-    Property extends keyof PluginSettings
-  >(
-    plugin: PluginBase<PluginSettings>,
-    property: Property,
-    options: BindValueComponentOptionsExtended<PluginSettings, UIValue, Property>
-  ): ValueComponentExType<UIValue, TValueComponent>;
-
-  /**
-   * Binds the ValueComponent to a property in the plugin settings.
-   *
-   * @typeParam Plugin - The type of the plugin that extends `PluginBase`.
-   * @typeParam Property - The key of the plugin setting that the component is bound to.
-   * @typeParam PluginSettings - The type of the plugin settings object.
-   * @param plugin - The plugin.
-   * @param property - The property key in `PluginSettings` to bind to the UI component.
-   * @param options - Configuration options.
-   * @returns The `ValueComponent` instance that was bound to the property.
-   */
-  public bind<
-    PluginSettings extends PluginSettingsBase,
-    Property extends keyof PluginSettings
-  >(
-    plugin: PluginBase<PluginSettings>,
-    property: Property,
-    options?: BindValueComponentOptions<PluginSettings, UIValue>
-  ): ValueComponentExType<UIValue, TValueComponent> {
-    type PropertyType = PluginSettings[Property];
-    const DEFAULT_OPTIONS: BindValueComponentOptionsExtended<PluginSettings, UIValue, Property> = {
-      componentToPluginSettingsValueConverter: (value): PropertyType => value as PropertyType,
-      pluginSettingsToComponentValueConverter: (value): UIValue => value as UIValue,
-      shouldAutoSave: true,
-      shouldShowValidationMessage: true
-    };
-
-    const optionsExt: BindValueComponentOptionsExtended<PluginSettings, UIValue, Property> = { ...DEFAULT_OPTIONS, ...options };
-    const pluginExt = plugin as unknown as PluginBase<PluginSettings>;
-    const pluginSettingsFn = (): PluginSettings => optionsExt.pluginSettings ?? pluginExt.settingsCopy;
-
-    const validate = async (uiValue?: UIValue): Promise<boolean> => {
-      if (!optionsExt.valueValidator) {
-        return true;
-      }
-      uiValue ??= this.valueComponent.getValue();
-      const errorMessage = await optionsExt.valueValidator(uiValue) as string | undefined;
-      const validatorElement = getValidatorElement(this.valueComponent);
-      if (validatorElement) {
-        validatorElement.setCustomValidity(errorMessage ?? '');
-        validatorElement.title = errorMessage ?? '';
-        validatorElement.toggleClass(CssClass.ValueComponentInvalid, !!errorMessage);
-        if (validatorElement.isActiveElement() && optionsExt.shouldShowValidationMessage) {
-          validatorElement.reportValidity();
-        }
-      }
-
-      return !errorMessage;
-    };
-
-    this.valueComponent
-      .setValue(optionsExt.pluginSettingsToComponentValueConverter(pluginSettingsFn()[property]))
-      .onChange(async (uiValue) => {
-        if (!await validate(uiValue)) {
-          return;
-        }
-        const pluginSettings = pluginSettingsFn();
-        pluginSettings[property] = optionsExt.componentToPluginSettingsValueConverter(uiValue);
-        if (optionsExt.shouldAutoSave) {
-          await pluginExt.saveSettings(pluginSettings);
-        }
-
-        await optionsExt.onChanged?.();
-      });
-
-    const validatorElement = getValidatorElement(this.valueComponent);
-    validatorElement?.addEventListener('focus', convertAsyncToSync(() => validate()));
-    validatorElement?.addEventListener('blur', convertAsyncToSync(() => validate()));
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    validatorsMap.set(this.valueComponent as ValueComponent<any>, () => validate());
-
-    invokeAsyncSafely(() => validate());
-    return this.asExtended();
-  }
-}
-
-/**
- * Extends a ValueComponent with additional functionality.
- *
- * @typeParam UIValue - The type of the value the component displays.
- * @typeParam TValueComponent - The type of the value component extending `ValueComponent`.
- * @param valueComponent - The value component to extend.
- * @returns The value component with extended functionality.
- */
-export function extend<UIValue, TValueComponent extends ValueComponentWithChangeTracking<UIValue>>(valueComponent: TValueComponent & ValueComponentWithChangeTracking<UIValue>): ValueComponentExType<UIValue, TValueComponent> {
-  return new ValueComponentEx<UIValue, TValueComponent>(valueComponent).asExtended();
-}
-
-/**
- * Revalidates a value component.
- *
- * @param valueComponent - The value component to revalidate.
- * @returns `true` if the value component is valid, `false` otherwise.
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function revalidate(valueComponent: ValueComponent<any>): Promise<boolean> {
-  const validator = validatorsMap.get(valueComponent);
-  if (validator) {
-    return await validator();
-  }
-
-  return true;
-}
-
 /**
  * Gets the validator element from a value component if it exists.
  *
  * @param valueComponent - The value component to get the validator element from.
  * @returns The validator element if it exists, or `null` if it does not.
  */
-function getValidatorElement<UIValue>(valueComponent: ValueComponentWithChangeTracking<UIValue>): null | ValidatorElement {
+export function getValidatorElement<UIValue>(valueComponent: ValueComponentWithChangeTracking<UIValue>): null | ValidatorElement {
   if (valueComponent instanceof DropdownComponent) {
     return valueComponent.selectEl;
   }
