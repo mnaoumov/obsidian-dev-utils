7b141b9c52e5a989c8edefafafc1efc8136b6a1e
Michael Naumov <mnaoumov@gmail.com>
2025-01-23 12:51:08 -0600

    Add fullRender

--- src/obsidian/Markdown.ts
+++ src/obsidian/Markdown.ts
@@ -3,11 +3,38 @@
  * This module provides utility functions for processing Markdown content in Obsidian.
  */
 
+import type { EmbeddableConstructor } from 'obsidian-typings';
+
+import { around } from 'monkey-around';
 import {
   App,
   Component,
   MarkdownRenderer
 } from 'obsidian';
+
+/**
+ * Render the markdown and embeds.
+ *
+ * @param app - The Obsidian app instance.
+ * @param markdown - The Markdown string to render.
+ * @param el - The HTMLElement to render to.
+ * @param sourcePath - The source path to resolve relative links.
+ * @param component - The Component instance.
+ */
+export async function fullRender(app: App, markdown: string, el: HTMLElement, sourcePath: string, component: Component): Promise<void> {
+  const uninstall = around(app.embedRegistry.embedByExtension, {
+    md: (next: EmbeddableConstructor): EmbeddableConstructor => (context, file, path) => {
+      context.displayMode = false;
+      return next(context, file, path);
+    }
+  });
+
+  try {
+    await MarkdownRenderer.render(app, markdown, el, sourcePath, component);
+  } finally {
+    uninstall();
+  }
+}
 
 /**
  * Converts Markdown to HTML.
