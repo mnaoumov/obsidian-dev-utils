94152ed83c5b2a48b771e3e44c4d8ce1fc87963c
Michael Naumov <mnaoumov@gmail.com>
2025-02-05 23:03:26 -0600

    Add setMin, setMax, setStep

--- src/obsidian/Components/TypedTextComponent.ts
+++ src/obsidian/Components/TypedTextComponent.ts
@@ -82,6 +82,28 @@ export abstract class TypedTextComponent<T> extends ValueComponent<T> implements
   }
 
   /**
+   * Sets the maximum value of the component.
+   *
+   * @param max - The maximum value.
+   * @returns The component.
+   */
+  public setMax(max: T): this {
+    this.inputEl.max = this.valueToString(max);
+    return this;
+  }
+
+  /**
+   * Sets the minimum value of the component.
+   *
+   * @param min - The minimum value.
+   * @returns The component.
+   */
+  public setMin(min: T): this {
+    this.inputEl.min = this.valueToString(min);
+    return this;
+  }
+
+  /**
    * Sets the placeholder of the component.
    *
    * @param placeholder - The placeholder to set.
@@ -89,6 +111,17 @@ export abstract class TypedTextComponent<T> extends ValueComponent<T> implements
    */
   public setPlaceholder(placeholder: string): this {
     this.textComponent.setPlaceholder(placeholder);
+    return this;
+  }
+
+  /**
+   * Sets the step value of the component.
+   *
+   * @param step - The step value.
+   * @returns The component.
+   */
+  public setStep(step: number): this {
+    this.inputEl.step = step.toString();
     return this;
   }
 
