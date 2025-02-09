f6a852883a423e10d6bccbdaa1b677aaec2c30f3
Michael Naumov <mnaoumov@gmail.com>
2025-02-08 19:32:57 -0600

    Switch to compile sass

--- src/obsidian/Components/EmailComponent.ts
+++ src/obsidian/Components/EmailComponent.ts
@@ -18,7 +18,7 @@ import { TypedTextComponent } from './TypedTextComponent.ts';
  *
  * In order to add the styles for the component, use {@link initPluginContext} in your plugin's `onload()` function.
  *
- * Alternatively, you can copy styles from {@link file://./../../../static/styles.css}.
+ * Alternatively, you can copy styles from {@link https://github.com/mnaoumov/obsidian-dev-utils/releases/latest/download/styles.css}.
  */
 export class EmailComponent extends TypedTextComponent<string> {
   /**
