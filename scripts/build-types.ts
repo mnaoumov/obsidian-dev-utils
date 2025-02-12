e64b420114296c58e17891ff772796b49d39a49f
Michael Naumov <mnaoumov@gmail.com>
2025-02-11 19:15:59 -0600

    Fix cts/mts refs

--- scripts/build-types.ts
+++ scripts/build-types.ts
@@ -1,10 +1,20 @@
-import { join } from '../src/Path.ts';
+import {
+  dirname,
+  join
+} from '../src/Path.ts';
 import { wrapCliTask } from '../src/scripts/CliUtils.ts';
 import { readdirPosix } from '../src/scripts/Fs.ts';
-import { cp } from '../src/scripts/NodeModules.ts';
+import {
+  mkdir,
+  readFile,
+  writeFile
+} from '../src/scripts/NodeModules.ts';
 import { ObsidianDevUtilsRepoPaths } from '../src/scripts/ObsidianDevUtilsRepoPaths.ts';
 import { execFromRoot } from '../src/scripts/Root.ts';
-import { trimEnd } from '../src/String.ts';
+import {
+  replaceAll,
+  trimEnd
+} from '../src/String.ts';
 
 await wrapCliTask(async () => {
   await execFromRoot('tsc --project ./tsconfig.types.json');
@@ -15,9 +25,12 @@ await wrapCliTask(async () => {
     }
 
     const fullSourcePath = join(ObsidianDevUtilsRepoPaths.Src, file);
-    const fullTargetBasePath = join(ObsidianDevUtilsRepoPaths.DistLib, trimEnd(file, ObsidianDevUtilsRepoPaths.DtsExtension));
+    const content = await readFile(fullSourcePath, 'utf-8');
 
-    await cp(fullSourcePath, fullTargetBasePath + ObsidianDevUtilsRepoPaths.DctsExtension);
-    await cp(fullSourcePath, fullTargetBasePath + ObsidianDevUtilsRepoPaths.DmtsExtension);
+    const fullTargetBasePath = join(ObsidianDevUtilsRepoPaths.DistLib, trimEnd(file, ObsidianDevUtilsRepoPaths.DtsExtension));
+    const parentDir = dirname(fullTargetBasePath);
+    await mkdir(parentDir, { recursive: true });
+    await writeFile(fullTargetBasePath + ObsidianDevUtilsRepoPaths.DctsExtension, replaceAll(content, '.ts\'', '.cjs\''));
+    await writeFile(fullTargetBasePath + ObsidianDevUtilsRepoPaths.DmtsExtension, replaceAll(content, '.ts\'', '.mjs\''));
   }
 });
