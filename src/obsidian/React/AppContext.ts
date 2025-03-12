533e3dfc3980cbec88ffeda417bd5b13e8e5cd5c
Michael Naumov <mnaoumov@gmail.com>
2025-03-11 19:45:39 -0600

    tsdoc

--- src/obsidian/React/AppContext.ts
+++ src/obsidian/React/AppContext.ts
@@ -4,8 +4,21 @@ import {
   useContext
 } from 'react';
 
+/**
+ * The context for the app.
+ *
+ * Usage:
+ * ```tsx
+ * <AppContext.Provider value={app}>
+ *   <YourComponent />
+ * </AppContext.Provider>
+ * ```
+ */
 export const AppContext = createContext<App | undefined>(undefined);
 
+/**
+ * The hook to use the app context.
+ */
 export const useApp = (): App => {
   const app = useContext(AppContext);
   if (!app) {
