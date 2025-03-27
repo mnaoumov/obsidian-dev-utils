147d5e1f530676bd6767c16f0943be138e24e581
Michael Naumov <mnaoumov@gmail.com>
2025-03-27 16:46:16 -0600

    Reuse type-fest

--- src/Object.ts
+++ src/Object.ts
@@ -39,22 +39,6 @@ enum TokenSubstitutionKey {
 }
 
 /**
- * Represents a constructor type for a given class `T` with arguments `Args`.
- *
- * @typeParam Type - The type of the instance being constructed.
- * @typeParam Args - The types of arguments the constructor accepts.
- */
-export type Constructor<Type, Args extends unknown[] = []> = new (...args: Args) => Type;
-
-/**
- * Extracts the keys of a given type `Type` that match a specific value type `Value`.
- *
- * @typeParam Type - The type from which to extract the keys.
- * @typeParam Value - The value type to match against.
- */
-export type KeysMatching<Type, Value> = { [Key in keyof Type]-?: Type[Key] extends Value ? Key : never }[keyof Type];
-
-/**
  * Options for converting an object to JSON.
  */
 export interface ToJsonOptions {
