a9a42e46545a41a5febaccc5aed0306aec51cce4
Michael Naumov <mnaoumov@gmail.com>
2025-04-01 10:38:31 -0600

    Add script examples

---
+++ static/scripts/examples/build.customPlugin.ts
@@ -0,0 +1,38 @@
+import type {
+  OnLoadResult,
+  Plugin
+} from 'esbuild';
+import type { CliTaskResult } from 'obsidian-dev-utils/ScriptUtils/CliUtils';
+import {
+  buildObsidianPlugin,
+  BuildMode
+} from 'obsidian-dev-utils/ScriptUtils/esbuild/ObsidianPluginBuilder';
+import { readFile } from 'obsidian-dev-utils/ScriptUtils/NodeModules';
+
+export async function invoke(): Promise<CliTaskResult> {
+  return await buildWithCustomPlugin();
+}
+
+export async function buildWithCustomPlugin(): Promise<CliTaskResult> {
+  return await buildObsidianPlugin({
+    mode: BuildMode.Production,
+    customEsbuildPlugins: [
+      customPlugin()
+    ]
+  });
+}
+
+function customPlugin(): Plugin {
+  return {
+    name: 'custom-plugin',
+    setup(build): void {
+      build.onLoad({ filter: /\.png$/ }, async (args): Promise<OnLoadResult> => {
+        const contents = await readFile(args.path);
+        return {
+          loader: 'dataurl',
+          contents
+        };
+      });
+    }
+  };
+}
