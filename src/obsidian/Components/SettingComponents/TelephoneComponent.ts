5ff1cc70f70b8ceb7286279d5739af5bb7684170
Michael Naumov <mnaoumov@gmail.com>
2025-04-16 15:15:50 -0600

    addPassword addTelephone

--- src/obsidian/Components/SettingComponents/TelephoneComponent.ts
+++ src/obsidian/Components/SettingComponents/TelephoneComponent.ts
@@ -4,6 +4,10 @@
  * Contains a component that allows the user to enter a telephone number.
  */
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { initPluginContext } from '../../Plugin/PluginContext.ts';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { SettingEx } from '../../SettingEx.ts';
 import type { TextBasedComponent } from './TextBasedComponent.ts';
 
 import { CssClass } from '../../../CssClass.ts';
@@ -13,6 +17,12 @@ import { TypedTextComponent } from './TypedTextComponent.ts';
  * A component that allows the user to enter a telephone number.
  *
  * It looks like regular text component on desktop but changes keyboard to digits only on mobile.
+ *
+ * You can add this component using {@link SettingEx.addTelephone}.
+ *
+ * In order to add the styles for the component, use {@link initPluginContext} in your plugin's `onload()` function.
+ *
+ * Alternatively, you can copy styles from {@link https://github.com/mnaoumov/obsidian-dev-utils/releases/latest/download/styles.css}.
  */
 export class TelephoneComponent extends TypedTextComponent<string> implements TextBasedComponent<string> {
   /**
