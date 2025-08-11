75f756b7faab135bc363fa80359be470f5297dbd
Michael Naumov <mnaoumov@gmail.com>
2025-08-11 01:41:31 -0600

    Lint

--- src/ScriptUtils/esbuild/preprocessPlugin.ts
+++ src/ScriptUtils/esbuild/preprocessPlugin.ts
@@ -79,7 +79,7 @@ export function preprocessPlugin(isEsm?: boolean): Plugin {
 
       build.initialOptions.banner ??= {};
       build.initialOptions.banner['js'] ??= '';
-      build.initialOptions.banner['js'] += `\n(${String((isEsm ? initEsm : initCjs))})();\n`;
+      build.initialOptions.banner['js'] += `\n(${String(isEsm ? initEsm : initCjs)})();\n`;
 
       build.onLoad({ filter: /\.(?:js|ts|cjs|mjs|cts|mts)$/ }, async (args) => {
         let contents = await readFile(args.path, 'utf-8');
