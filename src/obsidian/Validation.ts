468983a746b77f2d205961fa594114f3c726f74b
Michael Naumov <mnaoumov@gmail.com>
2025-08-13 13:57:56 -0600

    Rename validation variables

--- src/obsidian/Validation.ts
+++ src/obsidian/Validation.ts
@@ -6,6 +6,8 @@
 
 import { Platform } from 'obsidian';
 
+import { oneOf } from '../RegExp.ts';
+
 /**
  * Holds a validation message.
  */
@@ -40,6 +42,42 @@ export function isValidationMessageHolder(value: unknown): value is ValidationMe
 }
 
 /**
- * The forbidden file name characters for Obsidian.
+ * Matches characters that are not safe to use in file names within Obsidian.
  */
-export const OBSIDIAN_FORBIDDEN_FILE_NAME_CHARS_REG_EXP = /[#^[\]|]/g;
+export const OBSIDIAN_UNSAFE_FILENAME_CHARS = /[#^[\]|]/g;
+
+/**
+ * Windows-specific unsafe file name path characters.
+ */
+export const WINDOWS_UNSAFE_PATH_CHARS = /[*\\/<>:|?"]/g;
+
+/**
+ * Unix-specific unsafe file name path characters.
+ */
+export const UNIX_UNSAFE_PATH_CHARS = /[\0/]/g;
+
+/**
+ * Returns a regexp matching all unsafe characters in file names/paths.
+ *
+ * Includes both OS-specific restrictions and Obsidian-specific ones.
+ *
+ * @param isWindows - Whether to include Windows-specific restrictions. Defaults to `Platform.isWin`.
+ * @returns A regexp matching all unsafe characters in file names/paths.
+ */
+export function getOsAndObsidianUnsafePathCharsRegExp(isWindows?: boolean): RegExp {
+  return oneOf([
+    getOsUnsafePathCharsRegExp(isWindows),
+    OBSIDIAN_UNSAFE_FILENAME_CHARS
+  ]);
+}
+
+/**
+ * Returns a regexp matching characters that are not safe to use in file names/paths at the OS level.
+ *
+ * @param isWindows - Whether to include Windows-specific restrictions. Defaults to `Platform.isWin`.
+ * @returns A regexp matching characters that are not safe to use in file names/paths at the OS level.
+ */
+export function getOsUnsafePathCharsRegExp(isWindows?: boolean): RegExp {
+  isWindows ??= Platform.isWin;
+  return isWindows ? WINDOWS_UNSAFE_PATH_CHARS : UNIX_UNSAFE_PATH_CHARS;
+}
