0d9f3281dd12faf5b20c1f6a5725c1d6b24dbecb
Michael Naumov <mnaoumov@gmail.com>
2025-09-28 23:11:33 -0600

    docs: add missing tsdocs

--- src/obsidian/Commands/CommandBase.ts
+++ src/obsidian/Commands/CommandBase.ts
@@ -1,3 +1,9 @@
+/**
+ * @packageDocumentation
+ *
+ * Base classes for commands.
+ */
+
 import type {
   App,
   Command,
@@ -5,13 +11,38 @@ import type {
   Plugin
 } from 'obsidian';
 
+/**
+ * Options for creating a command.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export interface CommandBaseOptions<TPlugin extends Plugin> {
+  /**
+   * The icon to use for the command.
+   */
   icon: IconName;
+
+  /**
+   * The ID of the command.
+   */
   id: string;
+
+  /**
+   * The name of the command.
+   */
   name: string;
+
+  /**
+   * The plugin that the command belongs to.
+   */
   plugin: TPlugin;
 }
 
+/**
+ * Base class for commands.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export abstract class CommandBase<TPlugin extends Plugin> implements Command {
   public icon: IconName;
   public id: string;
@@ -19,6 +50,11 @@ export abstract class CommandBase<TPlugin extends Plugin> implements Command {
   protected readonly app: App;
   protected readonly plugin: TPlugin;
 
+  /**
+   * Creates a new command.
+   *
+   * @param options - The options for the command.
+   */
   public constructor(options: CommandBaseOptions<TPlugin>) {
     this.id = options.id;
     this.name = options.name;
@@ -27,19 +63,38 @@ export abstract class CommandBase<TPlugin extends Plugin> implements Command {
     this.app = this.plugin.app;
   }
 
+  /**
+   * Registers the command.
+   */
   public register(): void {
     this.plugin.addCommand(this);
   }
 }
 
+/**
+ * Base class for command invocations.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export abstract class CommandInvocationBase<TPlugin extends Plugin = Plugin> {
   protected readonly app: App;
   private lastCanExecuteResult?: boolean;
 
+  /**
+   * Creates a new command invocation.
+   *
+   * @param plugin - The plugin that the command invocation belongs to.
+   */
   public constructor(protected readonly plugin: TPlugin) {
     this.app = plugin.app;
   }
 
+  /**
+   * Invokes the command.
+   *
+   * @param checking - Is checking mode only. If `true`, only the check if the command can execute is performed. If `false`, the command is executed.
+   * @returns Whether the command was executed.
+   */
   public invoke(checking: boolean): boolean {
     this.lastCanExecuteResult = this.canExecute();
     if (!checking && this.lastCanExecuteResult) {
@@ -49,10 +104,18 @@ export abstract class CommandInvocationBase<TPlugin extends Plugin = Plugin> {
     return this.lastCanExecuteResult;
   }
 
+  /**
+   * Checks if the command can execute.
+   *
+   * @returns Whether the command can execute.
+   */
   protected canExecute(): boolean {
     return true;
   }
 
+  /**
+   * Executes the command.
+   */
   protected execute(): void {
     if (this.lastCanExecuteResult === undefined) {
       throw new Error('canExecute() must be called before execute()');
--- src/obsidian/Commands/NonEditorCommandBase.ts
+++ src/obsidian/Commands/NonEditorCommandBase.ts
@@ -1,13 +1,35 @@
+/**
+ * @packageDocumentation
+ *
+ * Base classes for non-editor commands.
+ */
+
 import type { Plugin } from 'obsidian';
 
 import type { CommandInvocationBase } from './CommandBase.ts';
 
 import { CommandBase } from './CommandBase.ts';
 
+/**
+ * Base class for non-editor commands.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export abstract class NonEditorCommandBase<TPlugin extends Plugin> extends CommandBase<TPlugin> {
+  /**
+   * Checks if the command can execute.
+   *
+   * @param checking - Is checking mode only. If `true`, only the check if the command can execute is performed. If `false`, the command is executed.
+   * @returns Whether the command can execute.
+   */
   public checkCallback(checking: boolean): boolean {
     return this.createCommandInvocation().invoke(checking);
   }
 
+  /**
+   * Creates a new command invocation.
+   *
+   * @returns The command invocation.
+   */
   protected abstract createCommandInvocation(): CommandInvocationBase;
 }
