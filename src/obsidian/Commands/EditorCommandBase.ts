e42cbe0d22c61b37511f93a00c76bc5914e5f724
Michael Naumov <mnaoumov@gmail.com>
2025-09-28 23:11:33 -0600

    docs: add missing tsdocs

--- src/obsidian/Commands/EditorCommandBase.ts
+++ src/obsidian/Commands/EditorCommandBase.ts
@@ -1,3 +1,9 @@
+/**
+ * @packageDocumentation
+ *
+ * Base classes for editor commands.
+ */
+
 import type {
   Editor,
   MarkdownFileInfo,
@@ -12,21 +18,51 @@ import {
   CommandInvocationBase
 } from './CommandBase.ts';
 
+/**
+ * Base class for editor commands.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export abstract class EditorCommandBase<TPlugin extends Plugin> extends CommandBase<TPlugin> {
   protected readonly menuItemName?: string;
   protected readonly menuSection?: string;
 
+  /**
+   * Checks if the command can execute.
+   *
+   * @param checking - Is checking mode only. If `true`, only the check if the command can execute is performed. If `false`, the command is executed.
+   * @param editor - The editor to check.
+   * @param ctx - The context of the command.
+   * @returns Whether the command can execute.
+   */
   public editorCheckCallback(checking: boolean, editor: Editor, ctx: MarkdownFileInfo | MarkdownView): boolean {
     return this.createEditorCommandInvocation(editor, ctx).invoke(checking);
   }
 
+  /**
+   * Registers the command.
+   */
   public override register(): void {
     super.register();
     this.plugin.registerEvent(this.app.workspace.on('editor-menu', this.handleEditorMenu.bind(this)));
   }
 
+  /**
+   * Creates a new editor command invocation.
+   *
+   * @param editor - The editor to create the command invocation for.
+   * @param ctx - The context of the command.
+   * @returns The command invocation.
+   */
   protected abstract createEditorCommandInvocation(editor: Editor, ctx: MarkdownFileInfo | MarkdownView): CommandInvocationBase;
 
+  /**
+   * Checks if the command should be added to the editor menu.
+   *
+   * @param _editor - The editor to check.
+   * @param _ctx - The context of the command.
+   * @returns Whether the command should be added to the editor menu.
+   */
   protected shouldAddToEditorMenu(_editor: Editor, _ctx: MarkdownFileInfo | MarkdownView): boolean {
     return false;
   }
@@ -50,13 +86,30 @@ export abstract class EditorCommandBase<TPlugin extends Plugin> extends CommandB
   }
 }
 
+/**
+ * Base class for editor command invocations.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export class EditorCommandInvocationBase<TPlugin extends Plugin> extends CommandInvocationBase<TPlugin> {
   protected activeFile!: TFile;
 
+  /**
+   * Creates a new editor command invocation.
+   *
+   * @param plugin - The plugin that the command invocation belongs to.
+   * @param editor - The editor to create the command invocation for.
+   * @param ctx - The context of the command.
+   */
   public constructor(plugin: TPlugin, protected readonly editor: Editor, protected readonly ctx: MarkdownFileInfo | MarkdownView) {
     super(plugin);
   }
 
+  /**
+   * Checks if the command can execute.
+   *
+   * @returns Whether the command can execute.
+   */
   public override canExecute(): boolean {
     if (!super.canExecute()) {
       return false;
--- src/obsidian/Commands/FileCommandBase.ts
+++ src/obsidian/Commands/FileCommandBase.ts
@@ -1,3 +1,9 @@
+/**
+ * @packageDocumentation
+ *
+ * Base classes for file commands.
+ */
+
 import type {
   Menu,
   Plugin,
@@ -10,17 +16,38 @@ import { TFile } from 'obsidian';
 import { CommandInvocationBase } from './CommandBase.ts';
 import { NonEditorCommandBase } from './NonEditorCommandBase.ts';
 
+/**
+ * Base class for file commands.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export abstract class FileCommandBase<TPlugin extends Plugin = Plugin> extends NonEditorCommandBase<TPlugin> {
   protected readonly menuItemName?: string;
   protected readonly menuSection?: string;
 
+  /**
+   * Registers the command.
+   */
   public override register(): void {
     super.register();
     this.plugin.registerEvent(this.app.workspace.on('file-menu', this.handleFileMenu.bind(this)));
   }
 
+  /**
+   * Creates a new file command invocation.
+   *
+   * @returns The command invocation.
+   */
   protected abstract override createCommandInvocation(): FileCommandInvocationBase<TPlugin>;
 
+  /**
+   * Checks if the command should be added to the file menu.
+   *
+   * @param _file - The file to check.
+   * @param _source - The source of the file.
+   * @param _leaf - The leaf to check.
+   * @returns Whether the command should be added to the file menu.
+   */
   protected shouldAddToFileMenu(_file: TFile, _source: string, _leaf?: WorkspaceLeaf): boolean {
     return false;
   }
@@ -50,9 +77,21 @@ export abstract class FileCommandBase<TPlugin extends Plugin = Plugin> extends N
   }
 }
 
+/**
+ * Base class for file command invocations.
+ *
+ * @typeParam TPlugin - The type of the plugin that the command belongs to.
+ */
 export abstract class FileCommandInvocationBase<TPlugin extends Plugin> extends CommandInvocationBase<TPlugin> {
   protected file!: TFile;
 
+  /**
+   * Invokes the command.
+   *
+   * @param checking - Is checking mode only. If `true`, only the check if the command can execute is performed. If `false`, the command is executed.
+   * @param file - The file to invoke the command for.
+   * @returns Whether the command was executed.
+   */
   public override invoke(checking: boolean, file?: TFile): boolean {
     if (file) {
       this.file = file;
@@ -60,6 +99,11 @@ export abstract class FileCommandInvocationBase<TPlugin extends Plugin> extends 
     return super.invoke(checking);
   }
 
+  /**
+   * Checks if the command can execute.
+   *
+   * @returns Whether the command can execute.
+   */
   protected override canExecute(): boolean {
     const file = (this.file as TFile | undefined) ?? this.app.workspace.getActiveFile();
     if (!file) {
