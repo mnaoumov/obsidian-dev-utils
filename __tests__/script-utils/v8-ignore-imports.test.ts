5a80eaf2ae6a7270e6702c0cf1f5f98607abec89
Michael Naumov <mnaoumov@gmail.com>
2026-03-07 20:01:48 -0600

    fix: update v8-ignore-imports test for named imports

--- __tests__/script-utils/v8-ignore-imports.test.ts
+++ __tests__/script-utils/v8-ignore-imports.test.ts
@@ -9,79 +9,87 @@ import {
   it
 } from 'vitest';
 
-import * as changeExtensionPlugin from '../../src/script-utils/bundlers/esbuild-impl/changeExtensionPlugin.ts';
-import * as copyToObsidianPluginsFolderPlugin from '../../src/script-utils/bundlers/esbuild-impl/copyToObsidianPluginsFolderPlugin.ts';
-import * as customEsbuildOptionsPlugin from '../../src/script-utils/bundlers/esbuild-impl/customEsbuildOptionsPlugin.ts';
-import * as Dependency from '../../src/script-utils/bundlers/esbuild-impl/dependency.ts';
-import * as fixEsmPlugin from '../../src/script-utils/bundlers/esbuild-impl/fixEsmPlugin.ts';
-import * as fixSourceMapsPlugin from '../../src/script-utils/bundlers/esbuild-impl/fixSourceMapsPlugin.ts';
-import * as ObsidianPluginBuilder from '../../src/script-utils/bundlers/esbuild-impl/obsidian-plugin-builder.ts';
-import * as preprocessPlugin from '../../src/script-utils/bundlers/esbuild-impl/preprocessPlugin.ts';
-import * as renameCssPlugin from '../../src/script-utils/bundlers/esbuild-impl/renameCssPlugin.ts';
-import * as svelteWrapperPlugin from '../../src/script-utils/bundlers/esbuild-impl/svelteWrapperPlugin.ts';
-import * as eslintConfig from '../../src/script-utils/linters/eslint-config.ts';
-import * as markdownlintCli2Config from '../../src/script-utils/linters/markdownlint-cli2-config.ts';
-import * as NodeModules from '../../src/script-utils/node-modules.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as StateFieldSpec from '../../src/codemirror/state-field-spec.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as DebugController from '../../src/debug-controller.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as CodeBlockMarkdownInformation from '../../src/obsidian/code-block-markdown-information.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as ValueComponentWithChangeTracking from '../../src/obsidian/components/setting-components/value-component-with-change-tracking.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as CustomTypeOptionsBase from '../../src/obsidian/i18n/custom-type-options-base.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as DefaultTranslationsBase from '../../src/obsidian/i18n/default-translations-base.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as PluginSettingsWrapper from '../../src/obsidian/plugin/plugin-settings-wrapper.ts';
+// eslint-disable-next-line import-x/no-namespace -- Namespace import needed to force v8 to load type-only module for coverage.
 import * as PluginTypesBase from '../../src/obsidian/plugin/plugin-types-base.ts';
+import { changeExtensionPlugin } from '../../src/script-utils/bundlers/esbuild-impl/changeExtensionPlugin.ts';
+import { copyToObsidianPluginsFolderPlugin } from '../../src/script-utils/bundlers/esbuild-impl/copyToObsidianPluginsFolderPlugin.ts';
+import { customEsbuildOptionsPlugin } from '../../src/script-utils/bundlers/esbuild-impl/customEsbuildOptionsPlugin.ts';
+import { getDependenciesToBundle } from '../../src/script-utils/bundlers/esbuild-impl/dependency.ts';
+import { fixEsmPlugin } from '../../src/script-utils/bundlers/esbuild-impl/fixEsmPlugin.ts';
+import { fixSourceMapsPlugin } from '../../src/script-utils/bundlers/esbuild-impl/fixSourceMapsPlugin.ts';
+import { BuildMode } from '../../src/script-utils/bundlers/esbuild-impl/obsidian-plugin-builder.ts';
+import { preprocessPlugin } from '../../src/script-utils/bundlers/esbuild-impl/preprocessPlugin.ts';
+import { renameCssPlugin } from '../../src/script-utils/bundlers/esbuild-impl/renameCssPlugin.ts';
+import { svelteWrapperPlugin } from '../../src/script-utils/bundlers/esbuild-impl/svelteWrapperPlugin.ts';
+import { obsidianDevUtilsConfigs } from '../../src/script-utils/linters/eslint-config.ts';
+import { obsidianDevUtilsConfig } from '../../src/script-utils/linters/markdownlint-cli2-config.ts';
+import { process } from '../../src/script-utils/node-modules.ts';
 
 describe('v8 ignore imports', () => {
   it('should load NodeModules so v8 processes its ignore comments', () => {
-    expect(NodeModules.process).toBeDefined();
+    expect(process).toBeDefined();
   });
 
   it('should load eslint.config so v8 processes its ignore comments', () => {
-    expect(eslintConfig.obsidianDevUtilsConfigs).toBeDefined();
+    expect(obsidianDevUtilsConfigs).toBeDefined();
   });
 
   it('should load Dependency so v8 processes its ignore comments', () => {
-    expect(Dependency).toBeDefined();
+    expect(getDependenciesToBundle).toBeDefined();
   });
 
   it('should load ObsidianPluginBuilder so v8 processes its ignore comments', () => {
-    expect(ObsidianPluginBuilder).toBeDefined();
+    expect(BuildMode).toBeDefined();
   });
 
   it('should load changeExtensionPlugin so v8 processes its ignore comments', () => {
-    expect(changeExtensionPlugin.changeExtensionPlugin).toBeDefined();
+    expect(changeExtensionPlugin).toBeDefined();
   });
 
   it('should load copyToObsidianPluginsFolderPlugin so v8 processes its ignore comments', () => {
-    expect(copyToObsidianPluginsFolderPlugin.copyToObsidianPluginsFolderPlugin).toBeDefined();
+    expect(copyToObsidianPluginsFolderPlugin).toBeDefined();
   });
 
   it('should load customEsbuildOptionsPlugin so v8 processes its ignore comments', () => {
-    expect(customEsbuildOptionsPlugin.customEsbuildOptionsPlugin).toBeDefined();
+    expect(customEsbuildOptionsPlugin).toBeDefined();
   });
 
   it('should load fixEsmPlugin so v8 processes its ignore comments', () => {
-    expect(fixEsmPlugin.fixEsmPlugin).toBeDefined();
+    expect(fixEsmPlugin).toBeDefined();
   });
 
   it('should load fixSourceMapsPlugin so v8 processes its ignore comments', () => {
-    expect(fixSourceMapsPlugin.fixSourceMapsPlugin).toBeDefined();
+    expect(fixSourceMapsPlugin).toBeDefined();
   });
 
   it('should load preprocessPlugin so v8 processes its ignore comments', () => {
-    expect(preprocessPlugin.preprocessPlugin).toBeDefined();
+    expect(preprocessPlugin).toBeDefined();
   });
 
   it('should load renameCssPlugin so v8 processes its ignore comments', () => {
-    expect(renameCssPlugin.renameCssPlugin).toBeDefined();
+    expect(renameCssPlugin).toBeDefined();
   });
 
   it('should load svelteWrapperPlugin so v8 processes its ignore comments', () => {
-    expect(svelteWrapperPlugin.svelteWrapperPlugin).toBeDefined();
+    expect(svelteWrapperPlugin).toBeDefined();
   });
 
   it('should load markdownlint-cli2-config so v8 processes its ignore comments', () => {
-    expect(markdownlintCli2Config.obsidianDevUtilsConfig).toBeDefined();
+    expect(obsidianDevUtilsConfig).toBeDefined();
   });
 
   it('should load DebugController so v8 processes its ignore comments', () => {
