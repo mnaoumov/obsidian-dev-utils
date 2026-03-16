eb73c00a4628909a8f1d1adc1110da5f6d64ba53
Michael Naumov <mnaoumov@gmail.com>
2026-03-16 16:38:56 -0600

    style: format mock adapter constructors and use dot notation

--- src/test-helpers/mocks/obsidian-typings/src/obsidian/augmentations/CapacitorAdapter.ts
+++ src/test-helpers/mocks/obsidian-typings/src/obsidian/augmentations/CapacitorAdapter.ts
@@ -14,13 +14,17 @@ import { mockImplementation } from '../../../../../mock-implementation.ts';
  * Patches CapacitorAdapter to expose `insensitive` from obsidian-typings.
  */
 export function mockCapacitorAdapter(): void {
-  mockImplementation(CapacitorAdapter.prototype, 'constructor__', function initCapacitorAdapter(this: CapacitorAdapter, originalImplementation, basePath: string, fs: unknown): void {
-    originalImplementation.call(this, basePath, fs);
-    Object.defineProperty(this, 'insensitive', {
-      get: (): boolean => this.insensitive__,
-      set: (value: boolean): void => {
-        ensureGenericObject(this)['insensitive__'] = value;
-      }
-    });
-  });
+  mockImplementation(
+    CapacitorAdapter.prototype,
+    'constructor__',
+    function initCapacitorAdapter(this: CapacitorAdapter, originalImplementation, basePath: string, fs: unknown): void {
+      originalImplementation.call(this, basePath, fs);
+      Object.defineProperty(this, 'insensitive', {
+        get: (): boolean => this.insensitive__,
+        set: (value: boolean): void => {
+          ensureGenericObject(this).insensitive__ = value;
+        }
+      });
+    }
+  );
 }
