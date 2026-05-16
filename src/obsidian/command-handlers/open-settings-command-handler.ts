3ef1aac9adb84516fba968f263f9011086b2b5fa
Michael Naumov <mnaoumov@gmail.com>
2026-05-15 19:27:43 -0600

    fix!: open settings

--- src/obsidian/command-handlers/open-settings-command-handler.ts
+++ src/obsidian/command-handlers/open-settings-command-handler.ts
@@ -4,31 +4,56 @@
  * Handles the "Open settings" command.
  */
 
-import type { PluginSettingsTabBase } from '../plugin/plugin-settings-tab.ts';
+import type {
+  App,
+  SettingTab
+} from 'obsidian';
 
 import { GlobalCommandHandler } from './global-command-handler.ts';
+
+/**
+ * Constructor parameters for {@link OpenSettingsCommandHandler}.
+ */
+export interface OpenSettingsCommandHandlerConstructorParams {
+  /**
+   * The Obsidian app instance.
+   */
+  readonly app: App;
+
+  /**
+   * The setting tab to open.
+   */
+  readonly settingTab: SettingTab;
+}
 
 /**
  * A command handler that opens the settings tab for a plugin.
  */
 export class OpenSettingsCommandHandler extends GlobalCommandHandler {
+  private readonly app: App;
+  private readonly settingTab: SettingTab;
+
   /**
    * Constructs a new instance.
    *
-   * @param pluginSettingsTab - The plugin settings tab to open.
+   * @param params - Constructor parameters.
    */
-  public constructor(private readonly pluginSettingsTab: PluginSettingsTabBase<object>) {
+  public constructor(params: OpenSettingsCommandHandlerConstructorParams) {
     super({
       icon: 'settings',
       id: 'open-settings',
       name: 'Open settings'
     });
+
+    this.app = params.app;
+    this.settingTab = params.settingTab;
   }
 
   /**
    * Executes the command.
    */
   public override execute(): void {
-    this.pluginSettingsTab.show();
+    this.app.setting.open();
+    this.app.setting.openTab(this.settingTab);
   }
 }
