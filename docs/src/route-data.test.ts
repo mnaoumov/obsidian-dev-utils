bacf44f250db1d9cf47e0aa72af9a21f4467988f
Michael Naumov <mnaoumov@gmail.com>
2026-07-15 12:59:26 -0600

    test(docs): cover the empty-slug root-page OG image fallback

--- docs/src/route-data.test.ts
+++ docs/src/route-data.test.ts
@@ -36,4 +36,27 @@ describe('onRequest', () => {
       { attrs: { content: 'https://mnaoumov.dev/obsidian-dev-utils/og/api/abort-controller/abortSignalAny.png', name: 'twitter:image' }, tag: 'meta' }
     ]);
   });
+
+  it('falls back to the index image when the slug is empty (root page)', () => {
+    vi.stubEnv('BASE_URL', '/obsidian-dev-utils/');
+    const head: unknown[] = [];
+    const context = {
+      locals: {
+        starlightRoute: {
+          head,
+          id: ''
+        }
+      },
+      url: new URL('https://mnaoumov.dev/obsidian-dev-utils/')
+    };
+
+    onRequest(context as Parameters<typeof onRequest>[0]);
+
+    expect(head).toEqual([
+      { attrs: { content: 'https://mnaoumov.dev/obsidian-dev-utils/og/index.png', property: 'og:image' }, tag: 'meta' },
+      { attrs: { content: '1200', property: 'og:image:width' }, tag: 'meta' },
+      { attrs: { content: '630', property: 'og:image:height' }, tag: 'meta' },
+      { attrs: { content: 'https://mnaoumov.dev/obsidian-dev-utils/og/index.png', name: 'twitter:image' }, tag: 'meta' }
+    ]);
+  });
 });
